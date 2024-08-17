a2bdc68a97ad42a9154fa777f7f0e7e7b8f2648a
surya00216 <suryaaa0216@gmail.com>
2024-08-18 01:37:57 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -14,14 +14,7 @@ import { getAuth } from "firebase/auth"
 // };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCMBUO0wJBWg0XjW06Vs90KyXBVXzIZXvs",
-  authDomain: "mota-318ad.firebaseapp.com",
-  databaseURL: "https://mota-318ad-default-rtdb.firebaseio.com",
-  projectId: "mota-318ad",
-  storageBucket: "mota-318ad.appspot.com",
-  messagingSenderId: "485568541914",
-  appId: "1:485568541914:web:90b45a3c444002a58061e3",
-  measurementId: "G-7D6RC7D31N"
+
 };
 
 const app = initializeApp(firebaseConfig);
